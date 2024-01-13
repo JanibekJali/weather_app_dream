7d3699d6f25c6a7968d557a0788d2f36b9fe3567
JanibekJali <janibekjali@gmail.com>
2024-01-13 14:18:30 +0600

    slider widget done

--- lib/app/presentation/home/home_page.dart
+++ lib/app/presentation/home/home_page.dart
@@ -8,6 +8,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  double _currentSliderValue = 0;
   List<String> time = [
     'now',
     '13:00',
@@ -35,11 +36,87 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+          leading: Image.asset('assets/images/icon.png'),
+          title: Image.asset('assets/images/point.png'),
+          actions: [Image.asset('assets/images/vector.png')],
+          centerTitle: true,
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Color(0xff16C4EA), Color(0xff7DE8FF)],
+                  begin: FractionalOffset(0.0, 0.0),
+                  end: FractionalOffset(0.5, 0.0),
+                  stops: [0.0, 2.0],
+                  tileMode: TileMode.clamp),
+            ),
+          )),
       backgroundColor: Colors.blue,
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Today',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w200),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              const InkWell(
+                  child: Text(
+                'Tomorrow',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w200),
+              )),
+              const SizedBox(
+                width: 10,
+              ),
+              InkWell(
+                  onTap: () {},
+                  child: const Text(
+                    'Next week',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w200),
+                  )),
+              const SizedBox(
+                width: 20,
+              ),
+              Center(
+                child: GestureDetector(
+                  onTap: () {},
+                  child: const Icon(
+                    Icons.arrow_forward_ios,
+                    color: Color(0xff9A938C),
+                  ),
+                ),
+              )
+            ],
+          ),
+          Slider(
+            activeColor: Colors.black,
+            inactiveColor: Colors.grey[800],
+            thumbColor: Colors.black,
+            value: _currentSliderValue,
+            min: 0,
+            max: 100,
+            onChanged: (double value) {
+              setState(() {
+                _currentSliderValue = value;
+              });
+            },
+          ),
           SizedBox(
-            height: MediaQuery.of(context).size.height * 0.9,
+            height: MediaQuery.of(context).size.height * 0.6,
           ),
           Expanded(
             child: ListView.builder(
